992bb1323ad4c0170c100c609102e40d9d5b9a83
hippolippo <joelhaftel@gmail.com>
2022-10-14 04:30:18 -0400

    Adding Node to Node Library

--- RestNodeUMM/Main.cs
+++ RestNodeUMM/Main.cs
@@ -18,10 +18,14 @@ namespace RestNodeUMM
     {
         static Harmony harmony;
         static AgentId restId;
+        static AgentGestalt gestalt;
+        static bool loadedNodeResources = false;
 
         public static bool Load(UnityModManager.ModEntry entry)
         {
+            Debug.Log("RestNodeUMM loaded");
             harmony = new Harmony(entry.Info.Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
             entry.OnToggle = OnToggle;
 
             restId = new AgentId();
@@ -29,24 +33,47 @@ namespace RestNodeUMM
             restId.guid = 1000;
             restId.displayName = "Rest";
             restId.agentGestaltId = (AgentGestaltEnum)1000;
-            AgentGestalt gestalt = (AgentGestalt)ScriptableObject.CreateInstance(typeof(AgentGestalt));
-            gestalt.properties = new Dictionary<int, AgentGestalt.Property>();
-            gestalt.ports = new Dictionary<int, AgentGestalt.Port>();
-            gestalt.ports[0] = new AgentGestalt.Port();
-            gestalt.ports[0].position = 5;
-            gestalt.ports[0].name = "URL";
-            gestalt.ports[0].onlyAppliesData = false;
-            gestalt.ports[0].description = "The url to post your REST request";
+            GetGestalt();
 
-            gestalt.properties[0] = new AgentGestalt.Property();
 
-            gestalt.agent = typeof(RestAgent);
-            
 
-            Holder.agentGestalts[(AgentGestaltEnum)1000] = gestalt;
+
+            //Holder.agentGestalts.Add((AgentGestaltEnum)1000, gestalt);
 
             return true;
 
+        }
+
+        public static AgentGestalt GetGestalt()
+        {
+            if (gestalt == null)
+            {
+                gestalt = (AgentGestalt)ScriptableObject.CreateInstance(typeof(AgentGestalt));
+                gestalt.advanced = true; // Make true if you want the node to say advanced when hovered over in node library, you should enable this if the node can be hard to use
+                gestalt.agent = typeof(RestAgent); // Very Important, the agent you set this to must be a class you define that can handle how the node acts
+                gestalt.componentCategory = AgentGestalt.ComponentCategories.Behavior; // As far as I know for now this should always be behavior for nodes
+                gestalt.description = "Creates a Rest POST Request"; //Description of the overall node behavior
+                gestalt.displayName = "Rest"; // should be same as AgentId.displayName
+                gestalt.id = (AgentGestaltEnum)1000; // should match AgentId.agentGestaltId
+                gestalt.nodeAlwaysRun = false; //I think this can make it act like the ticker
+                gestalt.nodeCategory = AgentCategoryEnum.Misc; // Category in node library
+                gestalt.processesModuleInterfaces = false; // This should only be on if you want to have it do something involving modules
+                gestalt.type = AgentGestalt.Types.Logic; // every node should have this
+
+                gestalt.properties = new Dictionary<int, AgentGestalt.Property>();
+
+                gestalt.ports = new Dictionary<int, AgentGestalt.Port>();
+                gestalt.ports[0] = new AgentGestalt.Port();
+                gestalt.ports[0].type = AgentGestalt.Port.Types.Command;
+                gestalt.ports[0].position = 1;
+                gestalt.ports[0].name = "URL";
+                gestalt.ports[0].onlyAppliesData = false;
+                gestalt.ports[0].description = "The url to post your REST request";
+
+
+                gestalt.properties[0] = new AgentGestalt.Property();
+            }
+            return gestalt;
         }
 
 
@@ -72,7 +99,7 @@ namespace RestNodeUMM
 
         public class RestAgent : Agent
         {
-            
+
             protected override void OnSetupFinished()
             {
                 this._v1 = this._runtimeProperties[0];
@@ -85,21 +112,23 @@ namespace RestNodeUMM
             }
 
             private AgentProperty _v1;
-            
+
 
         }
 
-
-        [HarmonyPatch(typeof(WorldController), "Update")]
-
-        class Patch
+        [HarmonyPatch(typeof(Resources), "LoadAll", new Type[]{typeof(string), typeof(Type)})]
+        class HolderAwakePatch
         {
-            public static void Postfix()
+            public static void Postfix(string path, Type systemTypeInstance, ref UnityEngine.Object[] __result)
             {
-                if (Input.GetKeyDown(KeyCode.L))
+                if (path == "Gestalts/Logic Agents" && systemTypeInstance == typeof(AgentGestalt) && !loadedNodeResources)
                 {
-
-                    SketchViewNode node = Controllers.worldController.visor.sketchView.AddNode(restId, false);
+                    int size = __result.Length;
+                    UnityEngine.Object[] temp = new UnityEngine.Object[size + 1];
+                    __result.CopyTo(temp, 0);
+                    temp[size] = GetGestalt();
+                    __result = temp;
+                    loadedNodeResources = true;
                 }
             }
         }
